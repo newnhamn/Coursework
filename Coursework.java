d68530ac38ebc82121a3aac5706a5b76edc63f2d
Newnham <newnhamn@egb070000048.lancs.local>
2018-03-19 17:17:04 +0000

    Got the image icons to fit to the buttons

--- Coursework.java
+++ Coursework.java
@@ -1,24 +1,41 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Coursework {
+
+ 
+  JButton[] button = new JButton[12]; 
 
   public Coursework() {
 
     JFrame a = new JFrame("Swingin' Simpsons");
-    a.setSize(1200, 900);
+    a.setSize(448, 363);
     a.setVisible(true);
     a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     JPanel panel = new JPanel();
-    panel.setSize(1200, 900);
+    panel.setSize(448, 363);
     a.setContentPane(panel);
     panel.setVisible(true);
 
-    ImageIcon img0 = new ImageIcon("bart0.jpg");
-    JButton i1 = new JButton(img0);
-    panel.add(i0);
-    i0.setSize(112,119);
-    i0.setVisible(true);
+    GridLayout g = new GridLayout(3, 4);
+    panel.setLayout(g);
+
+    ImageIcon[] imageArray = new ImageIcon[12];
+
+
+    for(int counter = 0; counter<12; counter++) {
+
+      imageArray[counter] = new ImageIcon("bart"+counter+".jpg");
+      button[counter] = new JButton(imageArray[counter]);
+      button[counter].setSize(112,119);
+      panel.add(button[counter]);
+      button[counter].setVisible(true);
+      
+    }
+
+        
+        panel.updateUI();
 
   }
 
